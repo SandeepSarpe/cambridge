5e51ed09a108f12f319304f7c2fb491b59f64159
sharad-vishe <sharadvishe251290@gmail.com>
2015-05-11 18:10:30 +0530

    commiting all the files

--- databases/etl/tables/department.sql
+++ databases/etl/tables/department.sql
@@ -3,6 +3,11 @@ BEGIN
 	create table if not exists etl.department(dept_id serial,dept_name varchar(100),primary key(dept_id));
 	if not exists(select true from information_schema.columns where table_name='department' and table_schema='etl' and column_name='company_name') then
 	       alter table etl.department add column company_name varchar(100);
-	end if;              
+	end if;   
+
+	if not exists(select true from information_schema.columns where table_name='department' and table_schema='etl' and column_name='company_address') then
+	       alter table etl.department add column company_address varchar(100);
+	end if; 
+
 END 
 $$;
